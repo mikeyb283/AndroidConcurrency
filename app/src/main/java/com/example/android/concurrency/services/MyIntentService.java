ec2c93ceaca2f4128f75b932c0cf1f3f22ebc57f
Michael Balling <mballing@students.mchenry.edu>
2020-03-18 08:53:08 -0500

    Finished Section 3.2

--- app/src/main/java/com/example/android/concurrency/services/MyIntentService.java
+++ app/src/main/java/com/example/android/concurrency/services/MyIntentService.java
@@ -3,14 +3,17 @@ package com.example.android.concurrency.services;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class MyIntentService extends IntentService {
+    public static final String SERVICE_MESSAGE = "ServiceMessage";
     private static final String ACTION_FOO = "com.example.android.concurrency.action.FOO";
 
     private static final String EXTRA_PARAM1 = "com.example.android.concurrency.extra.PARAM1";
     private static final String EXTRA_PARAM2 = "com.example.android.concurrency.extra.PARAM2";
     public static final String TAG = "CodeRunner";
+    public static final String MESSAGE_KEY = "message";
 
     public MyIntentService() {
         super("MyIntentService");
@@ -37,24 +40,31 @@ public class MyIntentService extends IntentService {
     }
 
     private void handleActionFoo(String param1, String param2) {
-        Log.i(TAG, "handleActionFoo: service started");
+        sendMessage( "handleActionFoo: service started");
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Log.i(TAG, "handleActionFoo: service finished");
+        sendMessage( "handleActionFoo: service finished");
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.i(TAG, "onCreate");
+        sendMessage( "onCreate");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "onDestroy");
+        sendMessage( "onDestroy");
+    }
+
+    private void sendMessage(String message) {
+        Intent intent = new Intent(SERVICE_MESSAGE);
+        intent.putExtra(MESSAGE_KEY, message);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+
     }
 }
