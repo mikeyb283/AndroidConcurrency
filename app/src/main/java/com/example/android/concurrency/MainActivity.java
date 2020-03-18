ca30881181468917f550b7e39599874560ccfa3d
Michael Balling <mballing@students.mchenry.edu>
2020-03-18 09:30:42 -0500

    Finished Section 3.3 and 3.4

--- app/src/main/java/com/example/android/concurrency/MainActivity.java
+++ app/src/main/java/com/example/android/concurrency/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.android.concurrency;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +22,28 @@ public class MainActivity extends AppCompatActivity {
     private TextView mLog;
     private ProgressBar mProgressBar;
 
+    //TODO: field to represent MyService
+    private MyService mService;
+
+    private final ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            //TODO: when binding to the service the service will be connected and the onServiceConnected method will be called
+            MyService.ServiceBinder serviceBinder = (MyService.ServiceBinder) service;
+            mService = serviceBinder.getService();
+            Log.i(TAG, "onServiceConnected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            if (mService != null) {
+                //TODO: release service to allow cleanup with garbage collection
+                mService = null;
+            }
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +57,24 @@ public class MainActivity extends AppCompatActivity {
         mLog.setText(R.string.lorem_ipsum);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent serviceIntent = new Intent(this, MyService.class);
+        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+        Log.i(TAG, "onStart: service bound");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(mServiceConn);
+        Log.i(TAG, "onStop: service unbound");
+    }
+
     //  Run some code, called from the onClick event in the layout file
     public void runCode(View v) {
-        log("Running code");
+        log(mService.getAValue());
     }
 
     //  Clear the output, called from the onClick event in the layout file
